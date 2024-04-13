ec6f6e13e521557734798dd89824bceba7f35692
bhavya01234 <bhavyamalik2020@gmail.com>
2024-04-13 17:24:05 +0530

    ignoring modules second
    gitignore update

--- EventFrontend/src/components/HeroSection.jsx
+++ EventFrontend/src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-scroll";
 const HeroSection = () => {
   return (
     <section className="hero">
-      <img src="/restaurant.jpg" alt="restaurant" />
+      <img src="/new_rest.jpg" alt="restaurant" />
       <div className="item">
         <h3>Dream Maker</h3>
         <div>
